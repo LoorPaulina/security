bd94fa83733ea08d590fe3ea1128c67019f65416
Paulina Loor Tubay <108084639+LoorPaulina@users.noreply.github.com>
2024-01-08 09:14:26 -0500

    repo

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/* REFERENCIA AL MÓDULO */
+const swaggerUi = require('swagger-ui-express');
+
+/* REFERENCIA AL ARCHIVO GENERADO */
+const swaggerFile = require('./swagger_output.json');
+
 var app = express();
 
 // view engine setup
@@ -27,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
