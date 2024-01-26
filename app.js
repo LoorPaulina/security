69f22f73900b7db8ce820818c879f2d21c7ce4a6
Paulina Loor Tubay <108084639+LoorPaulina@users.noreply.github.com>
2024-01-25 20:48:17 -0500

    change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
- /* MÓDULO dotenv */
+ /* MÓDULO dotenv e */
  const dotenv = require('dotenv');
 
  /* CARGA DE DATOS DE CONFIGURACION EN MEMORIA */
